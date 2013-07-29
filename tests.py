539db6a4bf25cd1e3a2edae18e78e254df1a7ee4
Daniel Greenfeld <pydanny@gmail.com>
2013-07-29 16:31:40 +0200

    Test harness prep

--- tests.py
+++ tests.py
@@ -1,5 +1,13 @@
 import unittest
 
+from django.conf import settings
+
+settings.configure(
+    DEBUG=True,
+    INSTALLED_APPS=[
+        "django.contrib.contenttypes",
+    ],
+)
 
 class TestCling(unittest.TestCase):
 
